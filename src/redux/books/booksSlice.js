72931395f9a3fcc5902519b79f712d7f4666c76a
Hyder <hyderali400@gmail.com>
2023-04-12 00:16:02 +0530

    fix linters

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -8,22 +8,10 @@ const booksSlice = createSlice({
   name: 'books',
   initialState,
   reducers: {
-    addBook: (state, action) => {
-      return {
-        ...state,
-        books: [...state.books, action.payload]
-      };
-    },
-    removeBook: (state, action) => {
-      const bookToRemove = action.payload;
-      return {
-        ...state,
-        books: state.books.filter((book) => book.id !== bookToRemove.id)
-      };
-    },
+    addBook: (state, action) => ({ ...state, books: [...state.books, action.payload] }),
+    removeBook: (state, action) => ({ ...state, books: state.books.filter((book) => book.id !== action.payload.id) }),
   },
 });
-
 
 export const { addBook, removeBook } = booksSlice.actions;
 export default booksSlice.reducer;
