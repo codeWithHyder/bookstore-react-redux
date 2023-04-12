c10745e165b68964275650e590b5b8bcd2dcedfd
Hyder <hyderali400@gmail.com>
2023-04-13 01:53:19 +0530

    fix linters

--- src/components/BookInput.js
+++ src/components/BookInput.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { useDispatch } from 'react-redux';
-import { addBook } from '../redux/books/booksSlice';
+import { addBook } from '../redux/books/booksSlice.js';
 
 const BookInput = () => {
   const [title, setTitle] = useState('');
